183c7461c4056bac66318e89d8cbc8a69f5af248
Thang Do <18thangd@gmail.com>
2023-02-12 14:21:15 +0700

    fix shiet

--- conflict.js
+++ conflict.js
@@ -1,7 +1,4 @@
 const items = [1, 2, 3, 4, 5, 6, 7]
-<<<<<<< HEAD
 console.log('branch master')
 const numberSix = items.find((item) => item === 6)
-=======
 const oddNumbers = items.filter((item) => item % 2 !== 0)
->>>>>>> 1398ea418590866d575d2db8bf86d79ed60708eb
