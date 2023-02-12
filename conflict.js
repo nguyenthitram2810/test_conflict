c726a9028446a6d84d36e54b479b5774181a9417
nguyenthitram2810 <nguyenthitram2010a@gmail.com>
2023-02-12 14:35:21 +0700

    fix conflict

--- conflict.js
+++ conflict.js
@@ -1,3 +1,8 @@
 const items = [1, 2, 3, 4, 5, 6, 7]
+<<<<<<< HEAD
 console.log('branch master')
 const numberSix = items.find((item) => item === 6)
+=======
+console.log(items)
+const oddNumbers = items.filter((item) => item % 2 !== 0)
+>>>>>>> branch_a
